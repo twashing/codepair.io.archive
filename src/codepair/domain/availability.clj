711fa29e7d319bce4299fe48e3911e6469d6bf25
Timothy Washington <twashing@interruptsoftware.com>
2015-02-14 16:56:53 +0000

    -- code and test for find-availability-by-title

--- src/codepair/domain/availability.clj
+++ src/codepair/domain/availability.clj
@@ -1,5 +1,6 @@
 (ns codepair.domain.availability
-  (:require [adi.core :as adi]))
+  (:require [adi.core :as adi]
+            [clojure.set :as set]))
 
 
 (defn add-availability [ds gname availability]
@@ -9,6 +10,19 @@
                {:group
                 {:name gname}}
                {:group/availabilities availability}))
+
+(defn find-availability-by-title
+  ([ds gname title]
+   (find-availability-by-title ds gname title [:ids {:availability :checked}]))
+
+  ([ds gname title opts]
+   (let [select-args (set/union [ds
+                                 {:availability
+                                  {:title title
+                                   :groups
+                                   {:name gname}}}
+                                 opts])]
+     (apply adi/select select-args))))
 
 (defn list-availabilities [ds gname]
   (adi/select ds
--- test/codepair/domain/availability_test.clj
+++ test/codepair/domain/availability_test.clj
@@ -48,11 +48,27 @@
                        (= '("Help Grokking Lambdas in Java" "Need Help Installing Purescript")
                           (map #(-> % :availability :title) b))))))
 
-#_(defspec test-retrieve-availability
+(defspec test-retrieve-availability
   5
   (prop/for-all [_ gen/int]
 
-                ))
+                (let [gname "codepair"
+                      ds (hlp/setup-db!)
+
+                      title "Need Help Installing Purescript"
+                      availability {:time :ongoing
+                                    :title title
+                                    :description "I'm new to Purescript, and want to get a basic development environment."
+                                    :tags #{{:name "purescript"} {:name "webdevelopment"} {:name "javascript"}}}
+                      a (av/add-availability ds gname availability)
+                      b (av/find-availability-by-title ds gname title)]
+
+                  (= b
+                     #{{:availability
+                        {:description
+                         "I'm new to Purescript, and want to get a basic development environment.",
+                         :title "Need Help Installing Purescript",
+                         :time :ongoing}}}))))
 
 (comment
 
