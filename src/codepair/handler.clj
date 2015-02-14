e3fe387a9ce8c1792fbf5836f60d82ec511898f8
Timothy Washington <twashing@interruptsoftware.com>
2015-02-14 23:23:50 +0000

    -- trouble-shooting handler functions

--- src/codepair/handler.clj
+++ src/codepair/handler.clj
@@ -3,7 +3,38 @@
             [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.util.response :as ring-resp]))
+            [ring.util.response :as ring-resp]
+            [bkell.config :as config]
+
+            [codepair.shell :as sh]))
+
+
+#_(defn gen-approutes
+  ([]
+   (gen-approutes (:dev (config/load-edn "config-codepair.edn"))))
+
+  ([env]
+
+   (sh/start {:shell {}
+              :spittoon {:env env
+                         :recreate? false}})
+
+   (defroutes app-routes
+
+     (GET "/" [:as req]
+
+          (-> (ring-resp/response (slurp (io/resource "public/index.html")))
+              (ring-resp/content-type "text/html")))
+
+     (GET "/foobar" [:as req]
+
+          (ring-resp/response (keys sh/system)))
+
+     (route/files "/")
+     (route/resources "/")
+     (route/not-found "Not Found"))))
+
+#_(gen-approutes)
 
 
 (defroutes app-routes
@@ -13,10 +44,13 @@
        (-> (ring-resp/response (slurp (io/resource "public/index.html")))
            (ring-resp/content-type "text/html")))
 
+  (GET "/foobar" [:as req]
+
+       (ring-resp/response (keys sh/system)))
+
   (route/files "/")
   (route/resources "/")
   (route/not-found "Not Found"))
-
 
 (def app
   (-> app-routes
