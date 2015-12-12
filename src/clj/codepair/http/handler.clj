a1ca237fa5c27a374b4cd1f7c50559009af79c31
Timothy Washington <twashing@interruptsoftware.com>
2015-12-12 17:46:44 +0000

    -- adding index.html -- serving up html from default locaation

--- src/clj/codepair/http/handler.clj
+++ src/clj/codepair/http/handler.clj
@@ -1,5 +1,6 @@
 (ns codepair.http.handler
-  (:require [bidi.ring :refer [make-handler]]
+  (:require [clojure.java.io :as io]
+            [bidi.ring :refer [make-handler]]
             [yada.yada :refer [yada]]
             [com.stuartsierra.component :as component]
             [aleph.http :as http]
@@ -8,40 +9,38 @@
             [manifold.bus :as b]
             [bkell.config :as config]))
 
+(defn generate-ice-handler [room]
+  
+  (fn [req]
+    ;; make ICE request
+    (let [conf (config/load-edn "config-codepair.edn")
+          resp @(http/post
+                 "https://service.xirsys.com/ice"
+                 {:body
+                  {:form-params
+                   {:ident "twashing"
+                    :secret (-> conf :dev :xirsys-token)
+                    :domain "codepair.io"
+                    :application "main"
+                    :room "main"
+                    :secure 1}}})]
+
+      (println resp)
+      
+      ;; subscribe requester to chatroom
+      (d/let-flow [socket (http/websocket-connection req)]
+        (s/connect
+         (b/subscribe room "main")
+         socket))
+      
+      ;; return ICE details
+      (select-keys resp [:status :body]))))
+
 (defn generate-handler [state]
-
   (let [room (:room state)]
-    
-    (make-handler ["/" {"" (fn [req]
-                             {:body "guten tag"})
-                        
-                        "room" (fn [req]
-
-                                 ;; make ICE request
-                                 (let [conf (config/load-edn "config-codepair.edn")
-                                       resp @(http/post
-                                             "https://service.xirsys.com/ice"
-                                             {:body
-                                              {:form-params
-                                               {:ident "twashing"
-                                                :secret (-> conf :dev :xirsys-token)
-                                                :domain "codepair.io"
-                                                :application "main"
-                                                :room "main"
-                                                :secure 1}}})]
-
-                                   (println resp)
-                                   
-                                   ;; subscribe requester to chatroom
-                                   (d/let-flow [socket (http/websocket-connection req)]
-                                               (s/connect
-                                                (b/subscribe room "main")
-                                                socket))
-                                   
-                                   ;; return ICE details
-                                   (select-keys resp [:status :body]))
-                                 
-                                 )}])))
+    (make-handler ["/" {"" (fn  [req] {:content-type "text/html"
+                                       :body (slurp (io/resource "public/index.html"))})
+                        "room" (generate-ice-handler room)}])))
 
 (defrecord HttpHandler []
   component/Lifecycle
